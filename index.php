c78d6b48c168992b3c051da23c693c7ac7a990a3
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2022-07-27 20:30:11 +0530

    Commit 6 27-07-2022

--- index.php
+++ index.php
@@ -8,11 +8,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Yash Agarwal</title>
 	<noscript><meta http-equiv="refresh" content="2; URL=https://www.google.com/"></noscript>
-	<link href="https://yashagarwal.epizy.com/" rel="canonical">
-	<link rel="icon" type="image/icon type" href="Images/YA3.png">
+	<link href="https://agarwalyash.epizy.com/" rel="canonical">
+	<link rel="icon" type="image/icon type" href="YA Logo.png">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Spectral|Rubik">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="" id="mainCSS1">
 	<link rel="stylesheet" type="text/css" href="" id="mainCSS2">
@@ -22,28 +21,34 @@
 	<noscript>
 		<h1>Please Enable Javascript</h1>
 	</noscript>
-	<!-- Main Navigation Section -->
-	<section>
-		<?php include "topnav.html"; ?>
-	</section>
-	<!-- Content Section -->
-	<section>
-		<div class="home" id="home">
-			<?php include "home.html"; ?>
-		</div>
-		<div class="bio" id="bio">
-			<?php include "bio.html"; ?>
-		</div>
-		<div class="work" id="work">
-			<?php include "work.html"; ?>
-		</div>
-	</section>
-	<section class="blank"></section>
-	<!-- Feedback Section -->
-	<section>
-		<?php include "feedback.html"; ?>
-	</section>
-	<section class="blank"></section>
+	<!-- Content -->
+	<div style="width: 100%; display: flex; flex-flow: row wrap; align-items: flex-start; align-items: flex-start;">
+		<!-- Main Navigation Section -->
+		<section class="nav1" id="nav1">
+			<?php include "topnav.html"; ?>
+		</section>
+		<!-- Content Section -->
+		<section style="width: 90%; margin-left: auto; margin-right: auto; display: flex; flex-flow: row-reverse wrap; align-items: space-around; justify-content: center;">
+			<!-- Home -->
+			<div class="home" id="home">
+				<?php include "home.html"; ?>
+			</div>
+			<!-- Bio -->
+			<div class="bio" id="bio">
+				<?php include "bio.html"; ?>
+			</div>
+			<!-- Work -->
+			<div class="work" id="work">
+				<?php include "work.html"; ?>
+			</div>
+			<!-- Feedback -->
+			<div class="feedback" id="feedback">
+				<?php include "feedback.html"; ?>
+			</div>
+		</section>
+	</div>
+	<div class="blank"></div>
+	<!-- Script to add Script -->
 	<script id="script1">
 		$(document).ready( function () {
 			$.ajaxSetup({cache: false});
@@ -53,10 +58,12 @@
 				dataType: 'json',
 				data: { folder: 'script1' },
 				success: function (argument) {
+					//alert(9);
 					let varible = document.createElement('script');
 					varible.src = argument.a;
 					let src = document.getElementById('script1');
 					src.appendChild(varible);
+					
 					$('#mainCSS1').prop('href', argument.b);
 
 					varible = document.createElement('script');
